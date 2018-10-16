81af4a6397d4fef499a6b61955fad853c38c1ac6
James-Chun <jchun00@stuy.edu>
2018-10-16 00:12:03 -0400

    didn't finish

---
+++ RationalNumber.java
@@ -0,0 +1,9 @@
+public RationalNumber extends RealNumber {
+    private int number
+    public void RationalNumber(int numerator, int denominator) {
+        number = numerator / denominator;
+    }
+    public int getNum() {
+        return 1;
+    }
+}
---
+++ RealNumber.java
@@ -0,0 +1,10 @@
+public class RealNumber {
+    private double number;
+    public void RealNumber(double num){
+        number = num;
+    }
+    public double getNum(){
+        return 1;
+    }
+    
+}
